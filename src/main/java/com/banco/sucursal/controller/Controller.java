ea2b2e349c708e62c81cd80b24f0b371359e603f
AFelipeLT16 <105022511+AFelipeLT16@users.noreply.github.com>
2023-09-08 00:53:10 -0500

    Update Controller.java
    Crear Cliente

--- src/main/java/com/banco/sucursal/controller/Controller.java
+++ src/main/java/com/banco/sucursal/controller/Controller.java
@@ -1,12 +1,20 @@
 package com.banco.sucursal.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 public class Controller {
-    @GetMapping(path = "/helloworld")
-    public String saludar() {
-        return "Hello world";
+    private ClienteRepository estudianteRepository;
+
+    public Controller(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+    @PostMapping(path = "/Banco/CrearCliente")
+    public String crearCliente(@RequestBody Cliente cliente) {
+        clienteRepository.save(cliente);
+        return "Cliente Creado Correctamente";
     }
 }
