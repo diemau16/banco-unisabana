08d42b8efa4aebbcc18130c0ae0e9a17230ca878
Joseph Mantilla <84591033+mvntillv@users.noreply.github.com>
2023-09-08 22:05:26 -0500

    LogicaTransaccion

---
+++ src/main/java/com/banco/sucursal/controller/TransaccionController.java
@@ -0,0 +1,34 @@
+package com.banco.sucursal.controller;
+import com.banco.sucursal.controller.dto.RespuestaDTO;
+import com.banco.sucursal.controller.dto.TransaccionDTO;
+import com.banco.sucursal.logica.TransaccionLogica;
+import com.banco.sucursal.persistencia.Cliente;
+import com.banco.sucursal.persistencia.Transaccion;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TransaccionController {
+
+    private TransaccionLogica transaccionLogica;
+
+    public TransaccionController(TransaccionLogica transaccionLogica) {
+        this.transaccionLogica = transaccionLogica;
+    }
+
+    @PostMapping(path = "/transaccion/nueva")
+    public RespuestaDTO realizarTransaccion(@RequestBody TransaccionDTO transaccion) {
+        try {
+            transaccionLogica.realizarTransaccion(transaccion);
+            return new RespuestaDTO("Transaccion realizada correctamente.");
+        } catch (IllegalArgumentException e){
+            return new RespuestaDTO("La transaccion no se pudo realizar: " + e.getMessage());
+        }
+    }
+
+    @GetMapping(path = "/transaccion/consultar")
+    public List<Transaccion> obtenerTransaccion() {
+        return transaccionLogica.obtenerTransacciones();
+    }
+}
--- src/main/java/com/banco/sucursal/logica/TransaccionRepository.java
+++
@@ -1,6 +0,0 @@
-package com.banco.sucursal.persistencia;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransaccionRepository extends JpaRepository<Transaccion, String> {
-}
