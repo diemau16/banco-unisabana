ee863f52a37294c3526c29a9f88a40637d72cb14
Diego Acosta <diemauacro@gmail.com>
2023-09-09 03:59:57 -0500

    Add encontrarProducto method to ProductoLogica

--- src/main/java/com/banco/sucursal/logica/ProductoLogica.java
+++ src/main/java/com/banco/sucursal/logica/ProductoLogica.java
@@ -3,7 +3,9 @@ package com.banco.sucursal.logica;
 import com.banco.sucursal.controller.dto.ProductoDTO;
 import com.banco.sucursal.persistencia.Producto;
 import com.banco.sucursal.persistencia.ProductoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -13,6 +15,16 @@ public class ProductoLogica {
 
     public ProductoLogica(ProductoRepository productoRepository){
         this.productoRepository = productoRepository;
+    }
+
+    public Producto encontrarProducto(int idProducto) {
+        List<Producto> listaProducto = obtenerProductos();
+        for (Producto producto : listaProducto) {
+            if (idProducto == producto.getIdProducto()) {
+                return producto;
+            }
+        }
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No existe el cliente.");
     }
 
     public void guardarProducto(ProductoDTO productoDTO) {
@@ -38,24 +50,14 @@ public class ProductoLogica {
     }
 
     public void desactivarProducto(int idProducto) {
-        List<Producto> listaProductos = obtenerProductos();
-        for (Producto producto : listaProductos) {
-            if (idProducto == producto.getIdProducto()) {
-                producto.setActivo(false);
-                productoRepository.save(producto);
-                return;
-            }
-        }
+        Producto producto = encontrarProducto(idProducto);
+        producto.setActivo(false);
+        productoRepository.save(producto);
     }
 
     public void activarProducto(int idProducto) {
-        List<Producto> listaProductos = obtenerProductos();
-        for (Producto producto : listaProductos) {
-            if (idProducto == producto.getIdProducto()) {
-                producto.setActivo(true);
-                productoRepository.save(producto);
-                return;
-            }
-        }
+        Producto producto = encontrarProducto(idProducto);
+        producto.setActivo(true);
+        productoRepository.save(producto);
     }
 }
