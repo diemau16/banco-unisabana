432e75efd85bc6d4ceb23526a311148601e77e13
Diego Acosta <diemauacro@gmail.com>
2023-10-01 02:17:09 -0500

    Ver

--- src/test/java/com/banco/sucursal/integration/ClienteControllerTest.java
+++ src/test/java/com/banco/sucursal/integration/ClienteControllerTest.java
@@ -2,14 +2,20 @@ package com.banco.sucursal.integration;
 
 import com.banco.sucursal.controller.dto.ClienteDTO;
 import com.banco.sucursal.controller.dto.RespuestaDTO;
+import com.banco.sucursal.persistencia.Cliente;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +32,11 @@ class ClienteControllerTest {
         ClienteDTO clienteDTO = new ClienteDTO("Carlos", "Gomez", 43);
         ResponseEntity<RespuestaDTO> respuesta = restTemplate.postForEntity("/cliente/agregar", clienteDTO, RespuestaDTO.class);
         assertEquals("Cliente creado correctamente.", respuesta.getBody().getRespuesta());
+    }
+
+    @Test
+    void obtenerClientes() {
+        ResponseEntity<List<Cliente>> respuesta = restTemplate.exchange("cliente/obtener", HttpMethod.GET, null, new ParameterizedTypeReference<List<Cliente>>() {});
+        assertEquals(HttpStatus.OK, respuesta.getStatusCode());
     }
 }
