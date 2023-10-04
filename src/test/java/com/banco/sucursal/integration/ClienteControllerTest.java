068fcf4fa2f4fe79af6718889a1aae877476f524
Diego Acosta <diemauacro@gmail.com>
2023-10-04 04:12:33 -0500

    Reset H2 before each test method

--- src/test/java/com/banco/sucursal/integration/ClienteControllerTest.java
+++ src/test/java/com/banco/sucursal/integration/ClienteControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = "test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ClienteControllerTest {
 
     @Autowired
